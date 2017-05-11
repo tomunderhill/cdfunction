9e3302dfefaa1398df461cf20872105d06e0893f
tomunderhill <t.m.underhill@gmail.com>
2017-05-11 15:58:50 +0000

    Commit compiled function project

---
+++ FunctionsAsWebProject/HelloHttpTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Threading.Tasks;
+
+namespace FunctionsLibraryProject
+{
+    public class HelloHttpTrigger
+    {
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function processed a request. RequestUri={req.RequestUri}");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+        }
+    }
+}
