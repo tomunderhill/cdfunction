4f845c70a0c6274bf9d979e2d1f236662fc8aaff
tomunderhill <t.m.underhill@gmail.com>
2017-05-25 10:48:26 +0000

    Create BlobOcrTrigger

---
+++ FunctionsAsWebProject/BlobOcrTrigger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace FunctionsLibraryProject
+{
+    public class BlobOcrTrigger
+    {
+        public static string Run(Stream myBlob, string name, TraceWriter log)
+        {
+            log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            var result = CallVisionAPI(myBlob);
+
+            var resultObject = JsonConvert.DeserializeObject<OcrResult>(result.Result);
+
+            var listOfBooks = new List<string>();
+
+            foreach(var region in resultObject.regions)
+            {
+                foreach (var line in region.lines)
+                {
+                    var keywords = string.Join("+", line.words.Select(x => x.text));
+
+                    var lookup = CallGoogleBooksAPI(keywords);
+
+                    var lookupObject = JsonConvert.DeserializeObject<GoogleBooksResult>(lookup.Result);
+
+                    if (lookupObject.items != null)
+                    {
+                        listOfBooks.Add(lookupObject.items.First().volumeInfo.title);
+                    }
+                }
+            }
+
+            return string.Join("/n/r ", listOfBooks);
+        }
+
+        static async Task<string> CallVisionAPI(Stream image)
+        {
+            using (var client = new HttpClient())
+            {
+                var content = new StreamContent(image);
+                var url = "https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr";
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Environment.GetEnvironmentVariable("Vision_API_Subscription_Key"));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                var httpResponse = await client.PostAsync(url, content);
+ 
+                if (httpResponse.StatusCode == HttpStatusCode.OK){
+                    return await httpResponse.Content.ReadAsStringAsync();
+                }
+            }
+            return null;
+        }
+
+        static async Task<string> CallGoogleBooksAPI(string keywords)
+        {
+            using (var client = new HttpClient())
+            {
+                var url = "https://www.googleapis.com/books/v1/volumes?q=" + keywords;
+                var httpResponse = await client.GetAsync(url);
+ 
+                if (httpResponse.StatusCode == HttpStatusCode.OK){
+                    return await httpResponse.Content.ReadAsStringAsync();
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ FunctionsAsWebProject/OcrObject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionsLibraryProject
+{
+    [Serializable]
+    public class OcrResult
+    {
+        public string language { get; set; }
+        public string textAngle { get; set; }
+        public string orientation { get; set; }
+        public IEnumerable<Region> regions { get; set; }
+    }
+
+    [Serializable]
+    public class Region
+    {
+        public string boundingBox { get; set; }
+        public IEnumerable<Line> lines { get; set; }
+    }
+
+    [Serializable]
+    public class Line
+    {
+        public string boundingBox { get; set; }
+        public IEnumerable<Word> words { get; set; }
+    }
+
+    [Serializable]
+    public class Word
+    {
+        public string boundingBox { get; set; }
+        public string text { get; set; }
+    }
+
+    [Serializable]
+    public class GoogleBooksResult
+    {
+        public string kind { get; set; }
+        public IEnumerable<GoogleBookItem> items { get; set; }
+    }
+
+    [Serializable]
+    public class GoogleBookItem
+    {
+        public string kind { get; set; }
+        public string id { get; set; }
+        public string etag { get; set; }
+        public string selfLink { get; set; }
+        public VolumeInfo volumeInfo { get; set; }
+    }
+
+    [Serializable]
+    public class VolumeInfo
+    {
+        public string title { get; set; }
+        public string publisher { get; set; }
+        public string publishedDate { get; set; }
+        public IEnumerable<string> authors { get; set; }
+        public string description { get; set; }
+        public IEnumerable<IndustryIdentifier> industryIdentifiers { get; set; }
+        public ImageLink imageLinks { get; set; }
+    }
+
+    [Serializable]
+    public class IndustryIdentifier
+    {
+        public string type { get; set; }
+        public string identifier { get; set; }
+    }
+
+    [Serializable]
+    public class ImageLink
+    {
+        public string smallThumbnail { get; set; }
+        public string thumbnail { get; set; }
+    }
+
+}
